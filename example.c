14444f1a3da76bce428973f71962c9734fa4018b
Abhishek-2310 <zephyrcr72310@gmail.com>
2023-08-17 18:58:51 -0400

    First commit

---
+++ example.c
@@ -0,0 +1,55 @@
+/* example.c:
+ *
+ *   Template code for implementing a simple task, using the ADD_TASK()
+ *   macro.  Also contains template code for a simple monitor command.
+ *
+ */
+
+#include <stdio.h>
+#include <stdint.h>
+
+#include "common.h"
+
+DMA_HandleTypeDef hdma_memtomem;
+
+void ExampleInit(void *data)
+{
+
+  /* Place Initialization things here.  This function gets called once
+   * at startup.
+   */
+  hdma_memtomem.Instance = DMA2_Stream0;
+  hdma_memtomem.Init.Channel = DMA_CHANNEL_0;
+  hdma_memtomem.Init.Direction = DMA_MEMORY_TO_MEMORY;
+  hdma_memtomem.Init.PeriphInc = DMA_PINC_ENABLE;
+  hdma_memtomem.Init.MemInc = DMA_MINC_ENABLE;
+
+}
+
+void ExampleTask(void *data)
+{
+
+  /* Place your task functionality in this function.  This function
+   * will be called repeatedly, as if you placed it inside the main
+   * while(1){} loop.
+   */
+}
+
+ADD_TASK(ExampleTask,  /* This is the name of the function for the task */
+	 ExampleInit,  /* This is the initialization function */
+	 NULL,         /* This pointer is passed as 'data' to the functions */
+	 0,            /* This is the number of milliseconds between calls */
+	 "This is the help text for the task")
+  
+
+ParserReturnVal_t CmdExample(int mode)
+{
+  if(mode != CMD_INTERACTIVE) return CmdReturnOk;
+
+  /* Put your command implementation here */
+  printf("Example Command\n");
+  
+  return CmdReturnOk;
+}
+
+ADD_CMD("example",CmdExample,"                Example Command")
