54c04bbe333c8315b10fd62fef930b9bb1cdb9a5
Abhishek-2310 <zephyrcr72310@gmail.com>
2023-09-13 17:18:47 -0400

    add comments to simpletask

--- simpletask.c
+++ simpletask.c
@@ -32,7 +32,7 @@ void LED_Init(void *data)
 
   GPIO_InitTypeDef GPIO_InitStruct;
 
-  GPIO_InitStruct.Pin = GPIO_PIN_5 | GPIO_PIN_6 | GPIO_PIN_7;
+  GPIO_InitStruct.Pin = GPIO_PIN_5;
   GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
   GPIO_InitStruct.Pull = GPIO_NOPULL;
   GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_HIGH;
